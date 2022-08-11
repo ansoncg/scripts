dd651a4bd90e0931f55edeca3526574bd8f30e6c
Anderson <cardoso_anderson@hotmail.com>
2022-08-11 01:52:47 -0300

    task script

--- others/virutal_cam.sh
+++ others/virutal_cam.sh
@@ -8,11 +8,14 @@ Arquivo com som:
 ffmpeg -stream_loop 3000 -re -i input.mp4 -map 0:v -f v4l2 /dev/video3 -f s16le -ar 44100 -ac 1 - > /tmp/virtualmic
 
 Stream com som:
-streamlink --stdout https://www.twitch.tv/esl_csgo best | ffmpeg -stream_loop 3000 -re -i pipe:0 -map 0:v -f v4l2 /dev/video3 -f s16le -ar 44100 -ac 1 - > /tmp/vmic
+streamlink --stdout https://www.twitch.tv/esl_csgo best | ffmpeg -stream_loop 3000 -re -i pipe:0 -map 0:v -f v4l2 /dev/video3 -f s16le -ar 44100 -ac 1 - > /tmp/virtualmic
 
 Video com som yt-dlp:
-yt-dlp "https://www.youtube.com/watch?v=JcXjbY7IX7I" -o - | ffmpeg -stream_loop 3000 -re -i pipe:0 -map 0:v -f v4l2 /dev/video3 -f s16le -ar 44100 -ac 1 - > /tmp/vmic
+yt-dlp "https://www.youtube.com/watch?v=JcXjbY7IX7I" -o - | ffmpeg -stream_loop 3000 -re -i pipe:0 -map 0:v -f v4l2 /dev/video3 -f s16le -ar 44100 -ac 1 - > /tmp/virtualmic
 
 
 # Remove audio
 pactl unload-module module-pipe-source
+
+# Remove cam
+sudo modprobe -r v4l2loopback
