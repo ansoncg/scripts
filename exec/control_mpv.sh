074de4c33324a98cb6f8236933bfb7015d4e4c28
Anderson <cardoso_anderson@hotmail.com>
2023-01-22 16:35:16 -0300

    renames, sigkill, ocr

--- exec/control_mpv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "pause" ]; then
-    echo cycle pause | socat - "/tmp/mpvsocket"
-elif [ "$1" == "next" ]; then
-    echo playlist-next | socat - "/tmp/mpvsocket"
-elif [ "$1" == "prev" ]; then
-    echo playlist-prev | socat - "/tmp/mpvsocket"
-fi
